9a96061d9273e1acba63521fb7a59574abd12a04
Meleneth <meleneth@gmail.com>
2026-02-28 01:45:37 -0800

    update what_is_this_script to have a global memes variable and disable the source when it finishes playing.  also use Array#cycle for looping

--- what_is_this_script.rb
+++ what_is_this_script.rb
@@ -1,55 +1,59 @@
 #!/usr/bin/env ruby
-require "debug"
-require "obsws"
-require "awesome_print"
+# frozen_string_literal: true
 
-def play_clip(clipname)
-    OBSWS::Requests::Client
-    .new(host: "localhost", port:4455)
-    .run do |client|
+require 'debug'
+require 'obsws'
+require 'awesome_print'
 
-        memes = Hash.new;
-
-        client.get_scene_item_list("Clips").scene_items.each{
-            |clip|
-            memes[clip[:sourceName]] = clip[:sceneItemId]
-
-        }
-        # ap memes
-        client.set_scene_item_enabled("Clips",memes[clipname],true);
-        client.set_input_audio_monitor_type(clipname,"OBS_MONITORING_TYPE_MONITOR_ONLY")
-
-        # binding.break
+$memes = {}
+OBSWS::Requests::Client
+  .new(host: 'localhost', port: 4455)
+  .run do |client|
+    client.get_scene_item_list('Clips').scene_items.each do |clip|
+      $memes[clip[:sourceName]] = clip[:sceneItemId]
     end
 end
 
-play_clip("why")
+def play_clip(clipname)
+  OBSWS::Requests::Client
+    .new(host: 'localhost', port: 4455)
+    .run do |client|
+      # ap memes
+      client.set_scene_item_enabled('Clips', $memes[clipname], true)
+      client.set_input_audio_monitor_type(clipname, 'OBS_MONITORING_TYPE_MONITOR_ONLY')
+
+    # binding.break
+  end
+end
+
 
 client = OBSWS::Events::Client
-    .new(host:"localhost",port:4455)
+         .new(host: 'localhost', port: 4455)
 
-
+# clips = ['fight']
 clips=['wat','mama','why','cowbell','shaggy','holycow','familystyle']
-index=0
 
-client.on :media_input_playback_ended do
-    |clip|
-    ap "Finished playing: #{clip.input_name}"
+cycler = clips.cycle
 
-    puts "before: #{index}"
-    play_clip(clips[index])
-    index += 1
-    puts "after: #{index}"
-    # play_clip(clips.sample)
-
+client.on :media_input_playback_ended do |clip|
+  ap "Finished playing: #{clip.input_name}"
+  OBSWS::Requests::Client
+    .new(host: 'localhost', port: 4455)
+    .run do |client|
+      client.set_scene_item_enabled('Clips', $memes[clip.input_name], false)
+    end
+  sleep 1
+  play_clip(cycler.next)
 end
+
+play_clip(clips[-1])
 
 # input = gets
 # puts input
 
 # client.run
 loop do
-    puts "sleeping"
-    sleep 1
-    puts "awaking"
+  puts 'sleeping'
+  sleep 1
+  puts 'awaking'
 end
