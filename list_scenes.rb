c2fe9214162084213b2c30cd7d2fb5522cdb42b9
Meleneth <meleneth@gmail.com>
2026-03-15 00:58:01 -0700

    Fixup list scenes to use the rails app's SceneIndex to list all scenes and all sources within the scene

--- list_scenes.rb
+++ list_scenes.rb
@@ -5,10 +5,15 @@ require 'debug'
 require 'obsws'
 require 'awesome_print'
 
+require_relative 'orinoco/app/lib/scene_index.rb'
+
 OBSWS::Requests::Client
   .new(host: 'localhost', port: 4455)
   .run do |client|
   client.get_scene_list.scenes.each do |scene|
-    puts scene
+    ap scene
+      clip_cache = SceneIndex.new(scene: scene[:sceneName])
+    clip_cache.refresh!(client)
+    ap clip_cache.by_name
   end
 end
