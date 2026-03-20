12c16dbc6400fc660f2dca3f208025559adfca25
Meleneth <meleneth@gmail.com>
2026-03-20 07:35:35 -0700

    bring in tailwind classes for the basic setup form, and add obs_config#create

--- orinoco/app/controllers/obs_configs_controller.rb
+++ orinoco/app/controllers/obs_configs_controller.rb
@@ -5,8 +5,19 @@ class ObsConfigsController < ApplicationController
     @obs_config = ObsConfig.first_or_initialize
   end
 
-  def update
+  def create
     @obs_config = ObsConfig.first_or_initialize
+    @obs_config.assign_attributes(obs_config_params)
+
+    if @obs_config.save
+      redirect_to(safe_return_to || edit_obs_config_path, notice: "OBS config saved.")
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @obs_config = ObsConfig.first
 
     if @obs_config.update(obs_config_params)
       redirect_to edit_obs_config_path, notice: "OBS config saved."
@@ -19,5 +30,12 @@ class ObsConfigsController < ApplicationController
 
   def obs_config_params
     params.expect(obs_config: [:host, :port])
+  end
+
+  def safe_return_to
+    uri = URI.parse(params[:return_to].to_s)
+    uri.path if uri.host.nil?
+  rescue URI::InvalidURIError
+    nil
   end
 end
--- orinoco/app/helpers/application_helper.rb
+++ orinoco/app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def cx(*parts)
+    parts.flatten.compact.reject(&:empty?).join(" ")
+  end
 end
