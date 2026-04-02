f03755c8d6db7d56c1377e463d91d556fcc87c3b
Meleneth <meleneth@gmail.com>
2026-04-01 23:26:50 -0700

    Add AffordanceConfig model so we can tell what ClipShow should be enabled for

--- railsapp/db/schema.rb
+++ railsapp/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_20_040601) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_01_055514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
+
+  create_table "affordance_configs", force: :cascade do |t|
+    t.jsonb "config", default: {}, null: false
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_affordance_configs_on_name", unique: true
+  end
 
   create_table "obs_configs", force: :cascade do |t|
     t.datetime "created_at", null: false
