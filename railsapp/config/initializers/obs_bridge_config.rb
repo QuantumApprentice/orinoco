551bb873766b088940c249ca608bb7a270654262
Meleneth <meleneth@gmail.com>
2026-04-03 20:13:43 -0700

    Fixup rails routes

--- railsapp/config/initializers/obs_bridge_config.rb
+++ railsapp/config/initializers/obs_bridge_config.rb
@@ -2,7 +2,7 @@
 return if ENV["SECRET_KEY_BASE_DUMMY"] == "1" || Rails.env.test?
 
 Rails.application.config.x.scoreboard.redis_url =
-  ENV.fetch("SCOREBOARD_REDIS_URL")
+  ENV.fetch("SCOREBOARD_REDIS_URL", "unset_scoreboard_redis_url")
 
 Rails.application.config.x.obs_bridge.bridge_id =
   ENV.fetch("OBS_BRIDGE_ID", "obs")
@@ -10,7 +10,7 @@ Rails.application.config.x.obs_bridge.bridge_id =
 Rails.application.config.x.obs_bridge.default_enabled = false
 
 Rails.application.config.x.obs_bridge.obs_host =
-  ENV.fetch("OBS_HOST")
+  ENV.fetch("OBS_HOST", "unset_obs_host")
 
 Rails.application.config.x.obs_bridge.obs_port =
   Integer(ENV.fetch("OBS_PORT", "4455"))
