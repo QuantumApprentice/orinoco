d2157318ac67b22cab3c0bd62758dbd89973e70f
meleneth <meleneth@gmail.com>
2026-04-26 18:23:30 -0700

    add a test devcontainer

--- railsapp/spec/rails_helper.rb
+++ railsapp/spec/rails_helper.rb
@@ -1,6 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+require 'spec_helper'
 require_relative '../config/environment'
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |file| require file }
 
