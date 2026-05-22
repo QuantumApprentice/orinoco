ae6d21d5505d95afd955f2255a25e7772f4ab8bb
QuantumApprentice <quantumapprentice@gmail.com>
2026-05-21 21:47:45 -0700

    added auto-scroll behavior to twitch chat so it scrolls from the bottom up now

---
+++ railsapp/app/javascript/controllers/autoscroll_controller.js
@@ -0,0 +1,24 @@
+// app/javascript/controllers/autoscroll_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.scrollToBottom()
+
+    this.observer = new MutationObserver(() => {
+      this.scrollToBottom()
+    })
+
+    this.observer.observe(this.element, { childList: true })
+  }
+
+  disconnect() {
+    this.observer?.disconnect()
+  }
+
+  scrollToBottom() {
+    requestAnimationFrame(() => {
+      this.element.scrollTop = this.element.scrollHeight
+    })
+  }
+}
