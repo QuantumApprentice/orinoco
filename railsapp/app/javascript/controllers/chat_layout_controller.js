94065d04ffdae3450d369ee79a14a23610ce4249
meleneth <meleneth@gmail.com>
2026-06-12 20:10:37 -0700

    Overlay chat header over feed

--- railsapp/app/javascript/controllers/chat_layout_controller.js
+++ railsapp/app/javascript/controllers/chat_layout_controller.js
@@ -4,22 +4,8 @@ export default class extends Controller {
     static targets = ["header", "body"]
 
     connect() {
-        this.syncLayout = this.syncLayout.bind(this)
-
-        this.resizeObserver = new ResizeObserver(this.syncLayout)
-        this.resizeObserver.observe(this.headerTarget)
-
-        window.addEventListener("resize", this.syncLayout)
-        this.syncLayout()
     }
 
     disconnect() {
-        this.resizeObserver?.disconnect()
-        window.removeEventListener("resize", this.syncLayout)
-    }
-
-    syncLayout() {
-        const headerHeight = Math.ceil(this.headerTarget.getBoundingClientRect().height)
-        this.bodyTarget.style.paddingTop = `${headerHeight}px`
     }
 }
